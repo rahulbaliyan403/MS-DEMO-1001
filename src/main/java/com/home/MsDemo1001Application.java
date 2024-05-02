6f648907bf7e9ade56eebf175419b6dedd50640e
rahulbaliyan403 <rahulbaliyan403@gmail.com>
2024-05-02 19:32:07 +0530

    second commit.

---
+++ src/main/java/com/home/MsDemo1001Application.java
@@ -0,0 +1,13 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDemo1001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDemo1001Application.class, args);
+	}
+
+}
